28389d5869bff32acf92d4f6d3f35fcb016a055c
jsrn <james.srn@gmail.com>
2018-10-24 23:37:42 +0100

    How many years?!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filter: "",
+      technologies: [
+        { name: "Ember.js", released: new Date(2011, 12, 8) },
+        { name: "Node.js", released: new Date(2009, 5, 27) },
+        { name: "React", released: new Date(2013, 5, 29) },
+        { name: "Ruby", released: new Date(1995, 12, 21) },
+        { name: "Ruby on Rails", released: new Date(2005, 12, 21) },
+        { name: "Vue", released: new Date(2014, 2, 1) }
+      ]
+    };
+
+    this.filterChanged = this.filterChanged.bind(this);
+    this.rowStyle = this.rowStyle.bind(this);
+  }
+
+  filterChanged(event) {
+    this.setState({
+      filter: event.target.value
+    })
+  }
+
+  rowStyle(name) {
+    let style = {};
+
+    if (name.toLowerCase().indexOf(this.state.filter.toLowerCase()) === -1) {
+      style.display = "none";
+    }
+
+    return style;
+  }
+
+  daysSince(date) {
+    return Math.floor((new Date() - date) / (365 * 60 * 24 * 1000 * 60));
+  }
+
   render() {
+    let rows = [];
+    let options = []
+
+    for (let i = 0; i < this.state.technologies.length; i++) {
+      rows.push(
+        <tr key={this.state.technologies[i].name} style={this.rowStyle(this.state.technologies[i].name)}>
+          <td>{this.state.technologies[i].name}</td>
+          <td>{this.daysSince(this.state.technologies[i].released)} years</td>
+        </tr>
+      );
+
+      options.push(
+        <option key={this.state.technologies[i].name} value={this.state.technologies[i].name} />
+      )
+    }
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>How long has <input id="tech-dropdown" type="search" list="technologies" placeholder="react, vue, ember, etc." onChange={this.filterChanged} /> existed?
+            <datalist id="technologies">
+              {options}
+            </datalist></h1>
         </header>
+
+        <main>
+          <table>
+            <thead>
+              <tr>
+                <th>Technology</th>
+                <th>How Long Has It Been Out?</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rows}
+            </tbody>
+          </table>
+        </main>
       </div>
     );
   }
