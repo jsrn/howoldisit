7ff68dfb9727fe8c1333f3dfb5eb1f248fd71586
Sarah-Jeanne Desrochers <sjdesrochers@challengeu.com>
2018-10-26 15:49:02 -0400

    handles "less than a year"

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     return style;
   }
 
-  daysSince(date) {
+  yearsSince(date) {
     return Math.floor((new Date() - date) / (365 * 60 * 24 * 1000 * 60));
   }
 
@@ -40,9 +40,10 @@ class App extends Component {
     let options = [];
 
     for (let i = 0; i < this.state.technologies.length; i++) {
+      let years = this.yearsSince(this.state.technologies[i].released)
       rows.push(
         <p key={this.state.technologies[i].name} style={this.rowStyle(this.state.technologies[i].name)}>
-          <strong>{this.state.technologies[i].name}</strong> has been out for <strong>{this.daysSince(this.state.technologies[i].released)} year{this.daysSince(this.state.technologies[i].released) === 1 ? '' : 's'}</strong>
+          <strong>{this.state.technologies[i].name}</strong> has been out for <strong>{years < 1 ? 'less than a' : years} year{years > 1 ? 's' : ''}</strong>
         </p>
       );
 
