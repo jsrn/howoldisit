6add4fccf601eee823562eac2eacba353a375ff8
kc1r74p <7422353+kc1r74p@users.noreply.github.com>
2018-10-28 21:11:00 +0100

    used more readable conditional rendering
    as suggested by disissid

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ class App extends Component {
     let techList = Object.assign([], this.state.technologies);
 
     if(this.state.sortBy === 'tech'){
-      techList.sort((a, b) => this.state.orderDesc ? a.name < b.name : a.name > b.name )
+      techList.sort((a, b) => this.state.orderDesc ? a.name.toLowerCase() < b.name.toLowerCase() : a.name.toLowerCase() > b.name.toLowerCase() )
     }else{
       techList.sort((a, b) => this.state.orderDesc ? a.released > b.released : a.released < b.released )
     }
@@ -72,31 +72,8 @@ class App extends Component {
 
 
   render() {
-    function SortOrderTechAsc(props) {
-      return (
-        <div className="inline"><p className="inline">Order by</p> <strong onClick={props.onClick}>Tech</strong><div className="arrow-up"></div>, <div className="inline" onClick={props.onClick}>Age</div></div>
-      );
-    }
-    function SortOrderTechDesc(props) {
-      return (
-        <div className="inline"><p className="inline">Order by</p> <strong onClick={props.onClick}>Tech</strong><div className="arrow-down"></div>, <div className="inline" onClick={props.onClick}>Age</div></div>
-      );
-    }
-  
-    function SortOrderAgeAsc(props) {
-      return (
-        <div className="inline"><p className="inline">Order by</p> <div className="inline" onClick={props.onClick}>Tech</div>, <strong onClick={props.onClick}>Age</strong><div className="arrow-up"></div></div>
-      );
-    }
-    function SortOrderAgeDesc(props) {
-      return (
-        <div className="inline"><p className="inline">Order by</p> <div className="inline" onClick={props.onClick}>Tech</div>, <strong onClick={props.onClick}>Age</strong><div className="arrow-down"></div></div>
-      );
-    }
-
     let rows = [];
     let options = [];
-    let sortOrder = {};
    
     for (let i = 0; i < this.state.technologies.length; i++) {
       let years = this.yearsSince(this.state.technologies[i].released)
@@ -112,15 +89,6 @@ class App extends Component {
       )
     }
 
-    sortOrder = <SortOrderTechAsc onClick={this.handleSort} />;
-    if(this.state.sortBy !== 'tech' && !this.state.orderDesc){
-      sortOrder = <SortOrderAgeAsc onClick={this.handleSort} />;
-    }else if(this.state.sortBy !== 'tech' && this.state.orderDesc){
-      sortOrder = <SortOrderAgeDesc onClick={this.handleSort} />;
-    }else if(this.state.sortBy === 'tech' && this.state.orderDesc){
-      sortOrder = <SortOrderTechDesc onClick={this.handleSort} />;
-    }
-
     return (
       <div className="App">
         <header className="App-header">
@@ -132,7 +100,20 @@ class App extends Component {
 
         <main>
           <p>This is a handy tool for tech recruiters who ask for fifteen years experience in technologies that have only existed for three months.</p>
-          {sortOrder}
+          
+          <div className="inline">
+            <p className="inline">Order by </p>
+            <div className={this.state.sortBy === "tech" ? "inline bold" : "inline"} onClick={this.handleSort}>
+              Tech
+            </div>
+            {this.state.sortBy === "tech" && <div className={this.state.orderDesc ? "arrow-down" : "arrow-up"} />}
+            <p className="inline">, </p>           
+            <div className={this.state.sortBy !== "tech" ? "inline bold" : "inline"} onClick={this.handleSort}>
+              Age
+            </div>
+            {this.state.sortBy !== "tech" && <div className={this.state.orderDesc ? "arrow-down" : "arrow-up"} />}
+          </div>    
+
           {rows}
 
           <p>Missing a technology? Find this repo on <a href="https://github.com/jsrn/howoldisit">GitHub</a>. Want a piece of me? Hurl abuse on <a href="https://twitter.com/jsrndoftime">Twitter</a>.</p>
