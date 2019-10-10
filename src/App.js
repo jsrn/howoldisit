06856cb681e37229971f0fb7df073fe6af45413a
Marcelo Iepsen <marcelo.iepsen@accedo.tv>
2019-10-10 09:12:02 +0200

    Added missing props at constructor

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import Icon from './icon';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       filter: "",
