acbac499bc3033d6c47ba4d71f2a57732b8fba5e
jsrn <jsrn@hey.com>
2020-10-01 14:46:00 +0100

    Move build to docs folder 'cause I'm lazy

---
+++ docs/precache-manifest.e8d40e029e38d05833c4ec21f2e4cdb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15666cd8695b4ad4537",
+    "url": "/howoldisit/static/js/runtime~main.b15666cd.js"
+  },
+  {
+    "revision": "0dd892b2bcbee80b2b01",
+    "url": "/howoldisit/static/js/main.0dd892b2.chunk.js"
+  },
+  {
+    "revision": "468182ff4a6f7232e1c6",
+    "url": "/howoldisit/static/js/1.468182ff.chunk.js"
+  },
+  {
+    "revision": "0dd892b2bcbee80b2b01",
+    "url": "/howoldisit/static/css/main.1f5e5b3e.chunk.css"
+  },
+  {
+    "revision": "6e3588b8f1ad34ae64506121b762b99e",
+    "url": "/howoldisit/index.html"
+  }
+];
